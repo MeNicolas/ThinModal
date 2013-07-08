d690e45d0639490ef99b55ceeb9a5da203a81925
MeNicolas <dalbard.nicolas@me.com>
2013-07-08 13:04:12 +0200

    Update 0.1
    Modal is more simple than before. Juste put class="modal" and
    SimpleModal do the rest!

--- jquery.SimpleModal.js
+++ jquery.SimpleModal.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	$('body').append('<div id="overlay"></div><div id="modal"></div><style>#overlay {position: fixed;z-index: 10000;top: 0px;left: 0px;height: 100%;width: 100%;background: #000;opacity: 0.2;display: none;}#modal { width: 600px; display: none; position: fixed; opacity: 1; z-index: 11000; left: 50%; margin-left: -270px; top: 200px; background-color:  #FFFFFF; border-radius: 2px; -moz-border-radius: 2px; -webkit-border-radius: 2px; box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.40); }</style>');
+	$('.modal').attr('url',$(this).attr('href')).attr('onclick','modal(this)').attr('href','javascript:void(0)');
 });
 function modal(modal){
 	
@@ -21,7 +22,3 @@ function modalclose(){
 }
 $(document).on("click", "#overlay", function(){modalclose()});
 $(document).on("click", "#close", function(){modalclose()});
-
-jQuery(document).ready(function() {
-  jQuery("a.timeago").timeago();
-});
