ad0ad7414ba4dcd80fb3db33d8ddf031ba2301bb
MeNicolas <dalbard.nicolas@me.com>
2013-07-08 12:54:17 +0200

    First version
    Code born!

---
+++ jquery.SimpleModal.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$('body').append('<div id="overlay"></div><div id="modal"></div><style>#overlay {position: fixed;z-index: 10000;top: 0px;left: 0px;height: 100%;width: 100%;background: #000;opacity: 0.2;display: none;}#modal { width: 600px; display: none; position: fixed; opacity: 1; z-index: 11000; left: 50%; margin-left: -270px; top: 200px; background-color:  #FFFFFF; border-radius: 2px; -moz-border-radius: 2px; -webkit-border-radius: 2px; box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.40); }</style>');
+});
+function modal(modal){
+	
+	var url = $(modal).attr('url');
+	var posting = $.post( url, { s: 1 } );
+
+	posting.done(function( data ) {
+		
+		$('#modal').html(data).fadeIn('fast');
+		$('#overlay').fadeIn();
+	});
+	
+}
+
+function modalclose(){
+	$('#modal').fadeOut(function(){$('#modal').empty();});
+	$('#overlay').fadeOut();
+
+}
+$(document).on("click", "#overlay", function(){modalclose()});
+$(document).on("click", "#close", function(){modalclose()});
+
+jQuery(document).ready(function() {
+  jQuery("a.timeago").timeago();
+});
