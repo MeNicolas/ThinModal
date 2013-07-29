40d7675f3d5e8abb27d2ba3271a2a38281d1bcf2
MeNicolas <dalbard.nicolas@me.com>
2013-07-29 18:18:08 +0200

    Bugs corrections

--- jquery.ThinModal.js
+++ jquery.ThinModal.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 	$('body').append('<div id="overlay"></div><div id="modal"></div><style>#overlay {position: fixed;z-index: 10000;top: 0px;left: 0px;height: 100%;width: 100%;background: #000;opacity: 0.2;display: none;}#modal { width: 600px; display: none; position: fixed; opacity: 1; z-index: 11000; left: 50%; margin-left: -270px; top: 200px; background-color:  #FFFFFF; border-radius: 2px; -moz-border-radius: 2px; -webkit-border-radius: 2px; box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.40); padding:10px;}#modalHeader{background:#F2F2F2;width:auto;height:30px;margin-left:-10px; margin-top:-10px; margin-right: -10px; margin-bottom: 10px; border-radius: 2px 2px 0px 0px;border-bottom:#d4d4d4 solid 1px;}#modalTitle{color:#7e7e7e;font-family:"Helvetica Neue",Arial,sans-serif;padding-top:7px;padding-left:10px;float:left;font-size:14px;}#modalClose{color:#7e7e7e;float:right;margin-right:10px;padding-top:5px;font-weight:bold;cursor:pointer;}#modalClose:hover{color:#444444;}</style>');
-	$('.modal').attr('url',$(this).attr('href')).attr('onclick','modal(this)').attr('href','javascript:void(0)');
-	$('.modalStyle').attr('onclick','modal(this,1)');
+	$('.modal').each(function(){
+		$(this).attr('url',$(this).attr('href')).attr('onclick','modal(this)').attr('href','javascript:void(0)');
+	});
+	$('.modalStyle').each(function(){
+		$(this).attr('onclick','modal(this,1)');
+	});
 });
 function modal(modal,style){
 	
